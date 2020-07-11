b386fb7481a7a56d56f5e277a22f5d479062b8e8
Hermano Leite <hplleite@gmail.com>
2020-07-11 15:22:07 -0300

    printing board

--- project.clj
+++ project.clj
@@ -2,8 +2,10 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [prismatic/schema "1.1.12"]]
   :main ^:skip-aot chess.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev     {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]}})
--- src/chess/core.clj
+++ src/chess/core.clj
@@ -1,7 +1,51 @@
 (ns chess.core
+  (:require [schema.core :as s])
   (:gen-class))
 
+(def abc ["a" "b" "c" "d" "e" "f" "g" "h"])
+
+(s/defn line-number :- s/Str
+  [column :- s/Int]
+  (str (- 8 column) " "))
+
+(s/defn column-letter :- s/Str
+  [line :- s/Int]
+  (str (nth abc (- line 1)) " "))
+
+(s/defn y-axis? :- s/Bool
+  [column :- s/Int
+   line :- s/Int]
+  (and (= line 0) (< column 8)))
+
+(s/defn x-axis? :- s/Bool
+  [column :- s/Int
+   line :- s/Int]
+  (and (= column 8) (> line 0)))
+
+(s/defn piece-at-position :- s/Str
+  [line :- s/Int
+   column :- s/Int]
+  (cond
+    (y-axis? line column)
+    (line-number line)
+
+    (x-axis? line column)
+    (column-letter column)
+
+    :else
+    "- "))
+
+(s/defn line
+  [line :- s/Int]
+  (map #(piece-at-position line %) (range 0 9)))
+
+(def print-line (comp println line))
+
+(def lines (map print-line (range 0 9)))
+
+(def print-board (doall lines))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "chess, mate!"
   [& args]
-  (println "Hello, World!"))
+  print-board)
