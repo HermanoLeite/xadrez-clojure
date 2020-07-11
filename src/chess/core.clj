380b1cc6c9944878b9402bd8acfebd9eec2d47d6
Hermano Leite <hplleite@gmail.com>
2020-07-11 20:05:56 -0300

    printing board - all in one file

--- src/chess/core.clj
+++ src/chess/core.clj
@@ -1,16 +1,19 @@
 (ns chess.core
-  (:require [schema.core :as s])
+  (:require [schema.core :as s]
+            [chess.schemata.piece :as s.piece]
+            [chess.game :as game])
+
   (:gen-class))
 
 (def abc ["a" "b" "c" "d" "e" "f" "g" "h"])
 
 (s/defn line-number :- s/Str
-  [column :- s/Int]
-  (str (- 8 column) " "))
+  [line :- s/Int]
+  (- 8 line))
 
 (s/defn column-letter :- s/Str
-  [line :- s/Int]
-  (str (nth abc (- line 1)) " "))
+  [column :- s/Int]
+  (nth abc (- column 1)))
 
 (s/defn y-axis? :- s/Bool
   [column :- s/Int
@@ -22,8 +25,43 @@
    line :- s/Int]
   (and (= column 8) (> line 0)))
 
+(s/defn piece-name :- s/Str
+  [{:keys [piece]} :- (s/maybe s.piece/Piece)]
+  (case piece
+    :pawn "P"
+    :bishop "B"
+    :knight "N"
+    :queen "Q"
+    :king "K"
+    :rook "R"
+    "-"))
+
+(s/defn inside-board? :- s/Bool
+  [board-line :- s/Int
+   board-column :- s/Int]
+  (and (>= board-line 0)
+       (< board-line 8)
+       (> board-column 0)
+       (< board-column 9)))
+
+(s/defn piece-at-position? :- s/Bool
+  [{{:keys [line column]} :position} :- s.piece/Piece
+   board-line :- s/Int
+   board-column :- s/Int]
+  (and (inside-board? board-line board-column)
+       (= (- 8 line) board-line)
+       (= column (column-letter board-column))))
+
+(s/defn find-piece-at-position :- (s/maybe s.piece/Piece)
+  [pieces :- (s/maybe s.piece/Piece)
+   line :- s/Int
+   column :- s/Int]
+  (let [piece (filter #(piece-at-position? % line column) pieces)]
+    (first piece)))
+
 (s/defn piece-at-position :- s/Str
-  [line :- s/Int
+  [piece :- (s/maybe s.piece/Piece)
+   line :- s/Int
    column :- s/Int]
   (cond
     (y-axis? line column)
@@ -33,19 +71,45 @@
     (column-letter column)
 
     :else
-    "- "))
+    (piece-name piece)))
+
+(s/defn last-column? :- s/Bool
+  [column :- s/Int]
+  (= 8 column))
+
+(s/defn color :- s/Str
+  [{:keys [color]} :- s.piece/Piece]
+  (if (= color :black)
+    "\u001B[31m"
+    "\u001B[37m"))
+
+(s/defn print-piece-at-position :- s/Str
+  [pieces :- [s.piece/Piece]
+   line :- s/Int
+   column :- s/Int]
+  (let [piece (find-piece-at-position pieces line column)
+        value (-> piece
+                  (piece-at-position line column)
+                  (str " "))]
+    (if (last-column? column)
+      (println (color piece) value)
+      (print (color piece) value))))
 
 (s/defn line
-  [line :- s/Int]
-  (map #(piece-at-position line %) (range 0 9)))
+  [pieces :- [s.piece/Piece]
+   line :- s/Int]
+  (doall (map #(print-piece-at-position pieces line %) (range 0 9))))
 
-(def print-line (comp println line))
+(s/defn print-board
+  [pieces :- [s.piece/Piece]]
+  (doall (map #(line pieces %) (range 0 9))))
 
-(def lines (map print-line (range 0 9)))
-
-(def print-board (doall lines))
+(s/defn board
+  [pieces :- [s.piece/Piece]]
+  (print-board pieces))
 
 (defn -main
   "chess, mate!"
   [& args]
-  print-board)
+  (let [pieces game/pieces]
+    (board pieces)))
---
+++ src/chess/schemata/piece.clj
@@ -0,0 +1,23 @@
+(ns chess.schemata.piece
+  (:require [schema.core :as s]))
+
+(def piece-type #{:pawn :bishop :knight :queen :king :rook})
+
+(def Type (apply s/enum piece-type))
+
+(def color #{:white :black})
+
+(def Color (apply s/enum color))
+
+(def position-skeleton {:line   {:schema s/Int :required true}
+                        :column {:schema s/Str :required true}})
+
+(s/defschema Position position-skeleton)
+
+(def piece-skeleton
+  {:position {:schema Position :required true}
+   :color    {:schema Color :required true}
+   :piece    {:schema Type :required true}})
+
+(s/defschema Piece piece-skeleton)
+
